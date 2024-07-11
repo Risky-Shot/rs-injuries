36e5958489b8088adedfa71a9ba23ebf56fc47b1
Jatin Kadian <kadianjatin@gmail.com>
2024-07-11 23:43:39 +0530

    Init

---
+++ data/misc.lua
@@ -0,0 +1,10 @@
+DamageType = {
+    DEFAULT = 1,
+    FALL = 2,
+    BLUNT = 4,
+    SHARP = 8,
+    GUN = 16,
+    FIRE = 32,
+    EXPLOSIVE = 64,
+    POISON = 128,
+}
---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'adamant'
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+game "rdr3"
+lua54 'yes'
+
+author 'RISKYSHOT'
+description 'A Minigame Resource. Thanks MaximilianAdF for code.'
+version '1.0'
+
+client_scripts {
+    'client/*.lua'
+}
+
+shared_scripts {
+    'config/*.lua'
+}
